804c4f7db5f37e53d1930b402bf39723f162f58c
a.suraev <a.suraev@vkteam.ru>
2025-09-28 16:58:56 +0500

    Fix styles

--- src/pages/game/ui/GamePage.tsx
+++ src/pages/game/ui/GamePage.tsx
@@ -1,5 +1,9 @@
 import styles from './GamePage.module.scss'
 
 export const GamePage = () => {
-  return <div className={styles.gamePage}>Game page</div>
+  return (
+    <div className={styles.gamePage}>
+      <p>Game page</p>
+    </div>
+  )
 }
