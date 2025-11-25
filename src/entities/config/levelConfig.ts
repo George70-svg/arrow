f51b4c7d955b13a1534327d087321a294d7a52a3
a.suraev <a.suraev@vkteam.ru>
2025-11-25 23:28:08 +0500

    Update level config

--- src/entities/config/levelConfig.ts
+++ src/entities/config/levelConfig.ts
@@ -9,12 +9,12 @@ export type Level = {
 export const levelConfig: Level[] = [
   {
     minScore: 5,
-    spawnTime: [4000, 6000],
+    spawnTime: [3000, 5000],
     enemies: [{ type: 'zombie', percent: 100 }],
   },
   {
     minScore: 10,
-    spawnTime: [4000, 6000],
+    spawnTime: [3000, 5000],
     enemies: [
       { type: 'zombie', percent: 70 },
       { type: 'orc', percent: 30 },
@@ -22,7 +22,7 @@ export const levelConfig: Level[] = [
   },
   {
     minScore: 20,
-    spawnTime: [4000, 6000],
+    spawnTime: [3000, 5000],
     enemies: [
       { type: 'zombie', percent: 50 },
       { type: 'orc', percent: 50 },
@@ -30,7 +30,7 @@ export const levelConfig: Level[] = [
   },
   {
     minScore: 30,
-    spawnTime: [4000, 6000],
+    spawnTime: [3000, 5000],
     enemies: [
       { type: 'zombie', percent: 40 },
       { type: 'orc', percent: 40 },
@@ -38,12 +38,30 @@ export const levelConfig: Level[] = [
     ],
   },
   {
-    minScore: 50,
+    minScore: 40,
     spawnTime: [2000, 4000],
     enemies: [
       { type: 'zombie', percent: 90 },
       { type: 'orc', percent: 5 },
       { type: 'goblin', percent: 5 },
+    ],
+  },
+  {
+    minScore: 50,
+    spawnTime: [2500, 4500],
+    enemies: [
+      { type: 'zombie', percent: 70 },
+      { type: 'orc', percent: 20 },
+      { type: 'goblin', percent: 10 },
+    ],
+  },
+  {
+    minScore: 60,
+    spawnTime: [2500, 4500],
+    enemies: [
+      { type: 'zombie', percent: 60 },
+      { type: 'orc', percent: 30 },
+      { type: 'goblin', percent: 10 },
     ],
   },
 ]
